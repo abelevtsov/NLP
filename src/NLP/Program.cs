7b791d4cd585e6c942ff5b616f6212bcb27f55cb
Aleksandr Belevtsov <belevtsov80@mail.ru>
2020-05-20 17:17:45 +0300

    package updated & some refinement

--- src/NLP/Program.cs
+++ src/NLP/Program.cs
@@ -1,62 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 
 using java.util;
 using java.io;
 using edu.stanford.nlp.pipeline;
 
-using Console = System.Console;
+using static System.Console;
 
 namespace NLP
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             // Path to the folder with models extracted from `stanford-corenlp-3.6.0-models.jar`
-            var jarRoot = @"C:\Users\abelevtsov\Documents\GitHub\NLP\stanford-corenlp-full-2015-12-09\stanford-corenlp-3.6.0-models";
+            var modelPath = args[0];
 
             // Text for processing
-            var text = "Kosgi Santosh sent an email to Stanford University. He didn't get a reply.";
+            var text = args[1];
 
-            // Annotation pipeline configuration
             var props = new Properties();
-            props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref"); // ToDo: NlpAction.tokenize | NlpAction.ssplit | NlpAction.pos | e.t.c.
-            props.setProperty("ner.useSUTime", "0");
 
-            // We should change current directory, so StanfordCoreNLP could find all the model files automatically
-            var curDir = Environment.CurrentDirectory;
-            Directory.SetCurrentDirectory(jarRoot);
-            var pipeline = new StanfordCoreNLP(props);
-            Directory.SetCurrentDirectory(curDir);
+            props.setProperty("annotators", $"{NlpAction.tokenize | NlpAction.ssplit | NlpAction.pos | NlpAction.lemma | NlpAction.ner | NlpAction.parse | NlpAction.dcoref}");
+            props.setProperty("ner.useSUTime", 0.ToString());
 
-            // Annotation
+            var pipeline = InitPipeline(modelPath, props);
+
             var annotation = new Annotation(text);
             pipeline.annotate(annotation);
 
-            // Result - Pretty Print
             using (var stream = new ByteArrayOutputStream())
             {
                 pipeline.prettyPrint(annotation, new PrintWriter(stream));
-                Console.WriteLine(stream.toString());
+                WriteLine(stream.toString());
                 stream.close();
             }
 
-            Console.ReadKey();
+            ReadKey();
+        }
+
+        /// <summary>
+        /// We should temporary change current directory, so StanfordCoreNLP could find all the model files automatically
+        /// </summary>
+        private static StanfordCoreNLP InitPipeline(string modelPath, Properties props)
+        {
+            var currentDir = Environment.CurrentDirectory;
+
+            Directory.SetCurrentDirectory(modelPath);
+            var pipeline = new StanfordCoreNLP(props);
+
+            Directory.SetCurrentDirectory(currentDir);
+
+            return pipeline;
         }
 
         [Flags]
         private enum NlpAction
         {
-            tokenize = 1,
-            ssplit = 2,
-            pos = 4,
-            lemma = 8,
-            ner = 16,
-            parse = 32,
-            dcoref = 64,
-            all = tokenize | ssplit | pos | lemma | ner | parse | dcoref
+            tokenize = 0x1,
+            ssplit = 0x2,
+            pos = 0x4,
+            lemma = 0x8,
+            ner = 0x10,
+            parse = 0x20,
+            dcoref = 0x40
         }
     }
 }
